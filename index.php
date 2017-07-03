3d6802200e00401f649c4b0167e371060b12fb60
alexeydudka <alexeydudka@gmail.com>
2017-07-03 17:01:59 +0300

    title and description for main page

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-<?php include_once "inc/header.php"; ?>
+<?php 
+	$title = "Alexey Dudka - my works";
+	$description = "The site is designed to description and demonstration of my works";
+	$keywords = "Alexey Dudka, alexeydudka, works, jQuery plugin, plugins, mobile interfaces";
+include_once "inc/header.php"; ?>
 
 <h2>Howdy… </h2>
 <p>I'm Alexey, a web developer from Ukraine.</p>
-<p>The site is designed to description and demonstration of my plugins, such as:</p>
+<p>The site is designed to description and demonstration of my works, such as:</p>
 <ul>
 	<li><a href="/mobilepopup" class="withhovering">Mobilepopup</a> - jQuery pop-up plugin</li>
 </ul>
