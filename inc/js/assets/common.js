459679ca0d9ae8da65039aa89e2ff7cda90f0e91
alexeydudka <alexeydudka@gmail.com>
2017-09-20 08:29:35 -0400

    updating of mobilepopup

--- inc/js/assets/common.js
+++ inc/js/assets/common.js
@@ -1,17 +1,65 @@
 hljs.initHighlightingOnLoad();
 $().ready(function(){
 	$("body").on("click",".popup-demo",function(){
-		console.log("sdf");
 		$.mobilepopup({
-			targetblock:".pop-up",
+			targetblock:".pop-up1",
 			width:"500px",
-			height:"300px",
-			onformsubmited: function(data, el){
-				$.mobilepopup('reload',{html:data});
+			height:"300px"
+		});
+		return false;
+	});
+
+	$("body").on("click",".get-demopopup2",function(){
+		$.mobilepopup('reload',{
+			targetblock:".pop-up2",
+			height:"400px"
+		});
+		return false;
+	});
+
+	$("body").on("click",".confirm-action",function(){
+		$.mobilepopup({
+			type:"confirm",
+			width:"500px",
+			height:"auto",
+			closeonoverflowclick: false,
+			fullscreeninmobile: false,
+			onconfirmed: function(el){
+				alert("Action confirmed");
 			}
 		});
 		return false;
 	});
+
+	$("body").on("click",".confirm-goto-link",function(e){
+		e.preventDefault();
+		var link = $(this);
+		$.mobilepopup({
+			type:"confirm",
+			width:"500px",
+			height:"auto",
+			closeonoverflowclick: false,
+			fullscreeninmobile: false,
+			onconfirmed: function(el){
+				location.href = link.attr("href");
+			}
+		});
+		return false;
+	});
+
+	$("body").on("click",".popup-form",function(){
+	    $.mobilepopup({
+			targetblock:".pop-up-form",
+			width:"500px",
+			height:"auto",
+			onformsubmited: function(data, el){
+				$.mobilepopup('reload',{html:data});
+			}
+		});
+	    return false;
+	});
+
+	
 
 	$("body").on("click",".toggle-menu-top",function(){
 		$(this).toggleClass("opened").closest("nav").find("> ul").toggleClass("opened-mobile-menu");
--- mobilepopup/index.php
+++ mobilepopup/index.php
@@ -15,7 +15,75 @@
 	<li>good decision for mobile site versions</li>
 </ul>
 <br><br>
-<h3>Example (<a href="" class="popup-demo">demo</a>):</h3>
+
+<h3>Simple example (<a href="" class="popup-demo">demo</a>):</h3>
+<pre><code class="language-javascript">
+$("body").on("click",".popup-demo",function(){
+    $.mobilepopup({
+        targetblock:".pop-up1",
+        width:"500px",
+        height:"300px"
+    });
+    return false;
+});
+
+$("body").on("click",".get-demopopup2",function(){
+    $.mobilepopup('reload',{
+        targetblock:".pop-up2",
+        width:"500px",
+        height:"400px"
+    });
+    return false;
+});</code></pre>
+
+<pre><code class="html">&#60;div class="pop-up1" style="display: none;"&#62;
+    &#60;div class="header"&#62;Demo popup 1&#60;/div&#62;
+    &#60;div class="content"&#62;
+        &#60;p&#62;
+            Demo popup 1 content
+        &#60;/p&#62;
+    &#60;/div&#62;
+    &#60;div class="footer"&#62;
+        &#60;a href="" class="button get-demopopup2"&#62;Open demo 2&#60;/a&#62;
+    &#60;/div&#62;
+&#60;/div&#62;
+
+&#60;div class="pop-up2" style="display: none;"&#62;
+    &#60;div class="header"&#62;Demo popup 2&#60;/div&#62;
+    &#60;div class="content"&#62;
+        &#60;p&#62;
+            Demo popup 2 content
+        &#60;/p&#62;
+    &#60;/div&#62;
+    &#60;div class="footer"&#62;
+        &#60;a href="" class="button close"&#62;Close pop-up&#60;/a&#62;
+    &#60;/div&#62;
+&#60;/div&#62;</code></pre>
+<div class="pop-up1" style="display: none;">
+    <div class="header">Demo popup 1</div>
+    <div class="content">
+        <p>
+            Demo popup 1 content
+        </p>
+    </div>
+    <div class="footer">
+        <a href="" class="button get-demopopup2">Open demo 2</a>
+    </div>
+</div>
+
+<div class="pop-up2" style="display: none;">
+    <div class="header">Demo popup 2</div>
+    <div class="content">
+        <p>
+            Demo popup 2 content
+        </p>
+    </div>
+    <div class="footer">
+        <a href="" class="button close">Close pop-up</a>
+    </div>
+</div>
+<br><br><br><br>
+<h3>Example of popup with form (<a href="" class="popup-demo">demo</a>):</h3>
 <pre><code class="language-javascript">$.mobilepopup({
 	targetblock:".pop-up",
 	width:300px,
@@ -28,13 +96,13 @@
 <div class="pop-up" style="display: none;">
 	<div class="header">We need your opinion!</div>
 	<div class="content">
-		<form action="ajax.php" class="popup-form">
+		<form action="ajax.php" class="mobilepopup-form">
 			<p>Please post any bugs, issues and suggestions about this page. This will help us to better enhance it.</p>
 			<textarea name="comment" placeholder="Write your comment"></textarea>
 		</form>
 	</div>
 	<div class="footer">
-		<a href="" class="submit-popup-form button">Send</a>
+		<a href="" class="submit-mobilepopup-form button">Send</a>
 	</div>
 </div>
 
@@ -50,6 +118,43 @@
 		&#60;a href="" class="submit-popup-form"&#62;Send&#60;/a&#62;
 	&#60;/div&#62;
 &#60;/div&#62;</code></pre>
+
+<br><br><br><br>
+<h3>Example of confirm action (<a href="" class="confirm-action">demo</a>):</h3>
+<pre><code class="language-javascript">
+$("body").on("click",".confirm-action",function(){
+	$.mobilepopup({
+		type:"confirm",
+		width:"500px",
+		height:"auto",
+		closeonoverflowclick: false,
+		fullscreeninmobile: false,
+		onconfirmed: function(el){
+			alert("Action confirmed");
+		}
+	});
+	return false;
+});</code></pre>
+<br><br><br><br>
+	<h3>Example go to link (<a href="http://alexeydudka.com/mobilepopup/" class="confirm-goto-link">demo</a>):</h3>
+<pre><code class="language-javascript">
+$("body").on("click",".confirm-goto-link",function(e){
+	e.preventDefault();
+	var link = $(this);
+	$.mobilepopup({
+		type:"confirm",
+		width:"500px",
+		height:"auto",
+		closeonoverflowclick: false,
+		fullscreeninmobile: false,
+		onconfirmed: function(el){
+			location.href = link.attr("href");
+		}
+	});
+	return false;
+});</code></pre>
+
+
 <br><br>
 <table class="table table-bordered">
 	<caption>Mobile popup functions</caption>
@@ -92,6 +197,21 @@
 		<td>Specifies the class or id of the block to display in the pop-up window. The block is cloned, that is, the block does not disappear from the page, and its clone is shown.</td>
 	</tr>
 	<tr>
+		<td>content</td>
+		<td>object</td>
+		<td>You can use plain text or html</td>
+	</tr>
+	<tr>
+		<td>type</td>
+		<td>type (text)</td>
+		<td>Specifies popup window type, by default 'standart'</td>
+	</tr>
+	<tr>
+		<td>confirmcontent</td>
+		<td>object</td>
+		<td>You can change content of confirm popup, by default {header: "Confirm your action", content: "Are you sure you want to continue?", buttonoktext: "Yes", buttonnotext: "Cancel"}</td>
+	</tr>
+	<tr>
 		<th colspan="3">The size of the window, for screens <= 767 this does not work, because the full screen</th>
 	</tr>
 	<tr>
@@ -118,6 +238,21 @@
 		<td>Clustom class for popup window</td>
 	</tr>
 	<tr>
+		<td>closeonoverflowclick</td>
+		<td>bool</td>
+		<td>Close popup on overflow click, by default true</td>
+	</tr>
+	<tr>
+		<td>shakeonoverflowclick</td>
+		<td>bool</td>
+		<td>Shake on overflow click if it disabled, by default true</td>
+	</tr>
+	<tr>
+		<td>fullscreeninmobile</td>
+		<td>bool</td>
+		<td>Fullscreen in mobile, by default true</td>
+	</tr>
+	<tr>
 		<th colspan="3">Callbacks</th>
 	</tr>
 	<tr>
@@ -135,12 +270,17 @@
 		<td>function</td>
 		<td>The function that is executed when the form inside the pop-up window is executed and the result of execution returns, a description of how this form is inside the pop-up window below</td>
 	</tr>
+	<tr>
+		<td>onconfirmed</td>
+		<td>function</td>
+		<td>The function that is executed when pressed "ok" in confirmation popup window</td>
+	</tr>
 </table>
 
 <p>For mobilepopup, you can quickly insert forms into a window and process them with ajax. What you need to do:</p>
 <ol>
-	<li>In the form inside the window, you need to specify a class <strong>popup-form</strong></li>
-	<li>The button that should send the form, you need to specify a class <strong>submit-popup-form</strong></li>
+	<li>In the form inside the window, you need to specify a class <strong>mobilepopup-form</strong></li>
+	<li>The button that should send the form, you need to specify a class <strong>submit-mobilepopup-form</strong></li>
 </ol>
 <p>Such a form is sent by ajax and after execution calls onformsubmited, the function returns the data that came as a result of executing the form</p>
 
